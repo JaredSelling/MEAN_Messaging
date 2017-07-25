722e00c0466d02915c44113d89a5d7666b706592
JaredSelling <jstephen2014@fau.edu>
2017-07-25 12:34:39 -0400

    Add save to db functionality

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from "./app.component";
 import { MessageComponent } from './messages/message.component';
@@ -10,6 +11,9 @@ import { MessagesComponent } from './messages/messages.component';
 import { AuthenticationComponent } from './auth/authentication.component';
 import { HeaderComponent } from '../app/header.component';
 import { routing } from './app.routing';
+import { LogoutComponent } from './auth/logout.component';
+import { SigninComponent } from './auth/signin.component';
+import { SignupComponent } from './auth/signup.component';
 
 @NgModule({
     declarations: [
@@ -19,9 +23,18 @@ import { routing } from './app.routing';
         MessageInputComponent,
         MessagesComponent,
         AuthenticationComponent,
-        HeaderComponent
+        HeaderComponent,
+        LogoutComponent,
+        SigninComponent,
+        SignupComponent
     ],
-    imports: [BrowserModule, FormsModule, routing],
+    imports: [
+      BrowserModule,
+      FormsModule,
+      routing,
+      ReactiveFormsModule,
+      HttpModule
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
