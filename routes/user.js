64a57ec91334c647239dc99ad452e42ea13650fb
JaredSelling <jstephen2014@fau.edu>
2017-07-26 13:08:34 -0400

    Add back end route for user signup

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcryptjs');
 
 var User = require('../models/user');
 
@@ -7,8 +8,20 @@ router.post('/', function (req, res, next) {
     var user = new User({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
-        password: req.body.password,
+        password: bcrypt.hashSync(req.body.password, 10),
         email: req.body.email
+    });
+    user.save(function(err, result) {
+      if(err) {
+          return res.status(500).json({
+              title: 'An error occurred',
+              error: err
+          });
+      }
+      res.status(201).json({
+          message: 'User created',
+          obj: result
+      });
     });
 });
 
