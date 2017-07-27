1d0235e240c3bad60673c0518b91b982bc990fea
JaredSelling <jstephen2014@fau.edu>
2017-07-27 14:47:20 -0400

    Add update and delete functionality

--- models/message.js
+++ models/message.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var User = require('./user');
+
 var schema = new Schema({
   content: {
     type: String, required: true
@@ -9,6 +11,14 @@ var schema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'User'
   }
+});
+
+//ensure that message array on user who created message is updated on delete
+schema.post('remove', function(message) {
+    User.findById(message.user, function(err, user) {
+        user.messages.pull(message);
+        user.save();
+    });
 });
 
 module.exports = mongoose.model('Message', schema);
